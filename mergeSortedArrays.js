c1e5c96ae3fe8c03ed2080ff249e13fc43eb9ea3
Brian Peterson <bepetersn@gmail.com>
2020-04-23 22:20:59 -0600

    Drop 'with'; drop one var

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -9,23 +9,21 @@
 var merge = function(nums1, m, nums2, n) {
     
     let numOfArr2
-    let numOfArr1
     let nextNumOfArr1
     let biggerThanPrev
     let smallerThanNext
     let nextIsZero
     let nextIsNotSet
     let preservesCorrectSort
-    let deleteContext = { deleteCount: 1 }
-    let deleteCount = 0
-    (for numOfArr2 of nums2) {
+    let deleteCount
+    for(numOfArr2 of nums2) {
         
-        for(let i=0; i<nums1.length; i++) {
+        for(let index=0; index<nums1.length; index++) {
             
+            nextNumOfArr1 = nums1[index+1]
+
             // if we can establish it preserves the correct sort,
-            numOfArr1 = nums[i]
-            nextNumOfArr1 = nums[i+1]
-            biggerThanPrev = numOfArr1 <= numOfArr2
+            biggerThanPrev = nums1[index] <= numOfArr2
             smallerThanNext = numOfArr2 <= nextNumOfArr1
             preservesCorrectSort = biggerThanPrev && smallerThanNext
             nextIsZero = nextNumOfArr1 === 0
@@ -33,10 +31,10 @@ var merge = function(nums1, m, nums2, n) {
             if(preservesCorrectSort || nextIsNotSet || nextIsZero) {
                 
                 // insert numOfArr2 at the next position; 
-                // also, delete the zero if there is one
-                with(deleteContext) {
-                  nums1.splice(i+1, deleteCount, numOfArr2)
-                }
+                // also, delete the zero if there is one there
+                deleteCount = +nextIsZero
+                // TODO: does this delete the right thing when there is a zero?
+                nums1.splice(index+1, deleteCount, numOfArr2)
                 break
             }
         }
